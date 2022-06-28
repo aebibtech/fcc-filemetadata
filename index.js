84351e40b60dc1ff458ffc50f69bb7d82fa586e7
Paul Abib S. Camano <paul.camano@gmail.com>
2022-06-28 21:06:30 +0000

    working code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile'),
+  function (req, res) {
+    try {
+      res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+      });
+    } catch(err) {
+      res.send(400);
+    }
+  }
+);
 
 
 const port = process.env.PORT || 3000;
